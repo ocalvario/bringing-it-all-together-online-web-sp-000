df46e3deea922cf8ef7ef74d4bdffb7ba870cd4c
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 00:33:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,5 +7,17 @@ class Dog
     @name = name
     @breed = breed
   end 
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER 
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
 end
