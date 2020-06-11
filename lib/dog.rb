d36a1304aacede8079836834c8bef963c527d5ce
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 00:41:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,10 +35,12 @@ class Dog
         VALUES (?,?)
       SQL
 
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
+  
+  
 
   
   def self.create(name, breed)
