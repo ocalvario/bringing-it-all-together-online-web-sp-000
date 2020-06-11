8130df7d9231f13b066bec30f525d499d32b60e0
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 00:41:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,8 +41,8 @@ class Dog
   end
   
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
 
   end
 
